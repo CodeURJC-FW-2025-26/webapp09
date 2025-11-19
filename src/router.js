8c7d2bd11807de4428d636886e09b8676cc2594b
sguindal <sergio.ggpb@gmail.com>
2025-11-19 13:00:06 +0100

    Confirmacion2

--- src/router.js
+++ src/router.js
@@ -367,3 +367,4 @@ router.get('/clothe/:id/review/:idReview/edit', async (req,res) => {
     let review = clothe.reviews.find(r => r.id === Number(req.params.idReview) )
     return res.render('edit_review', {clothe, review})
 })
+
